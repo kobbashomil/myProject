ead7a6c8ae300e0ef728618775f7b0400ca0988e
kobashomil@gmail.com <kobbasshomi@gmail.com>
2024-07-16 22:20:05 +0300

    tuple-list-str

---
+++ tuple & list & str.py
@@ -0,0 +1,17 @@
+def tuple():
+   age=(40,50,60)
+   print(age,type(age))
+   print(age[0])
+tuple()  
+print("==============")  
+def lis():
+    toll=[30,40,50,60]
+    toll.append(70)
+    toll.insert(1,90)
+    print(toll,type(toll))
+
+lis()                                         
+def string():
+    name="bashomil"  
+    print(name[0:4])
+string()    
